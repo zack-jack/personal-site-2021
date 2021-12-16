951a14491ac03928b2668348b569d09c6bd21eee
zack-jack <zjackson@redventures.com>
2021-12-15 22:39:54 -0500

    remove boilerplate from nextjs cli

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="application-name" content="Zack Jackson" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="Personal website for Zack Jackson front End Web Developer"
+        />
+        <title>Zack Jackson | Front End Developer</title>
+      </Head>
+
+      <div id="app" className="flex flex-col min-h-screen">
+        <main id="main" className="flex flex-col grow">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&family=Source+Sans+Pro:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
