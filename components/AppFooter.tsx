30488c1e4513d50b6098a6535547848d926b9f1e
zack-jack <zjackson@redventures.com>
2021-12-20 16:54:50 -0500

    add footer content

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -1,7 +1,33 @@
 import React, { ReactElement } from 'react';
+import Link from 'next/link';
+import Emoji from './Emoji';
+import SocialIconGithub from './SocialIconGithub';
+import SocialIconLinkedIn from './SocialIconLinkedIn';
 
 const AppFooter = (): ReactElement => {
-  return <footer id="footer">This is the footer.</footer>;
+  return (
+    <footer id="footer" className="app-footer">
+      <div className="app-footer__social-wrapper">
+        <Link href="https://github.com/zack-jack">
+          <a target="_blank" rel="noopener noreferrer">
+            <SocialIconGithub />
+            <span className="sr-only">Github</span>
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/zack-jackson/">
+          <a target="_blank" rel="noopener noreferrer" className="ml-10">
+            <SocialIconLinkedIn />
+            <span className="sr-only">LinkedIn</span>
+          </a>
+        </Link>
+      </div>
+      <div className="app-footer__made-with">
+        <span>Made with</span>
+        <Emoji label="heart emoji" symbol={'\u{1F5A4}'} className="ml-1" />
+        <span className="ml-2">in Charlotte, NC.</span>
+      </div>
+    </footer>
+  );
 };
 
 export default AppFooter;
